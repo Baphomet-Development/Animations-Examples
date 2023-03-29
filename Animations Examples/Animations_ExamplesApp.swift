af604a97450d7910b3f4d632efb705d96c44b4d3
Jason Lacy <jdlacy@gmail.com>
2023-03-28 22:20:25 -0700

    Initial Commit

---
+++ Animations Examples/Animations_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_ExamplesApp.swift
+//  Animations Examples
+//
+//  Created by Jason on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Animations_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations Examples
+//
+//  Created by Jason on 3/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
